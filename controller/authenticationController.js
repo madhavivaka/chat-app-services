6273d127d36c0cca6c6075e1e8cc8659fcdac9c5
balaji <balajiganesh.mca@gmail.com>
2018-04-08 23:41:22 +0530

    xvx

---
+++ controller/authenticationController.js
@@ -0,0 +1,24 @@
+var http=require("http");
+var authService=require("../services/authenticationService");
+
+exports.userLogin=function(req,res,next){
+   console.log('in auth controller',authService);
+   authService.validateLogin(req,function(response){
+   	console.log('rrrrrrrrrrrrrr',response);
+	   res.json(response);
+   });
+}
+
+exports.userRegister=function(req,res,next){
+   console.log('userRegister',req.body);
+   authService.userRegister(req,function(response){
+	   res.json(response);
+   });
+}
+
+exports.getOnlineUsers=function(req,res,next){
+   console.log('getOnlineUsers');
+   authService.getOnlineUsers(req,function(response){
+	   res.json(response);
+   });
+}
