fe3c9d07a23815c54108b13f94e2395855b2076e
Madhavi Vaka <dommeti.madhavivaka@gmail.com>
2018-04-12 19:06:17 +0530

    message services

---
+++ services/messagesService.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var mongoose = require('mongoose');
+var User = mongoose.model('user');
+var message = mongoose.model('message');
+var room = mongoose.model('room');
+
+
+
+
+exports.saveMessage=function(req,callback){
+	console.log('userRegister srvice',req.body.reqParams);
+	var data=req.body.reqParams;
+	User.findOne({"username":req.body.reqParams.username},function(err,response){
+		console.log('responseresponse',response);
+		if(err || response == null){
+          var user=new User({
+          	firstName:data.firstName,
+          	lastName:data.lastName,
+          	email:data.email,
+          	username:data.username,
+          	password:data.password
+
+          });
+          user.save(function(err){
+		      callback({status:"Success"});
+          });
+		}
+		else{
+			callback({status:"Fail",message:"User Already Exists"});
+		}
+		
+	});
+
+}
+
+
