e774877e67c6192832c82e49f271cf8b57482724
balaji <balajiganesh.mca@gmail.com>
2018-04-19 22:51:01 +0530

    message

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var authController=require('../controller/authenticationController');
+var chatController=require('../controller/chatController');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +14,8 @@ router.post('/api/userLogin', authController.userLogin);
 router.post('/api/userRegister', authController.userRegister);
 
 router.post('/api/getOnlineUsers', authController.getOnlineUsers);
+
+router.post('/api/saveMessage', chatController.saveMessage);
 
 
 
